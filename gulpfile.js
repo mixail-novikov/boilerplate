59595f014d0a762ad33aa933d98cf3479f0a9cc4
Mikhail Novikov <mikhail.novikov@travelline.ru>
2015-06-09 12:56:26 +0300

    add gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
 
-// Gulp Sass Task
 gulp.task('sass', function() {
-    gulp.src('./css/**/*.{scss,sass}')
-        // Initializes sourcemaps
-        .pipe(sourcemaps.init())
-        .pipe(sass({
+    gulp.src('./css/style.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
             errLogToConsole: true
         }))
-        // Writes sourcemaps into the CSS file
-        .pipe(sourcemaps.write())
+        .pipe($.sourcemaps.write())
         .pipe(gulp.dest('./css'));
 });
 
 // Watch scss folder for changes
 gulp.task('watch', function() {
-    // Watches the scss folder for all .scss and .sass files
-    // If any file changes, run the sass task
-    gulp.watch('./css/**/*.{scss,sass}', ['sass'])
+    gulp.watch('css/**/*.{scss,sass}', ['sass']);
 });
 
 // Creating a default task
