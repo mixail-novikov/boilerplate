d97ccd643952eace2fe20507e4a606fd54fccc08
Mikhail Novikov <mixail.novikov@gmail.com>
2015-09-13 02:27:50 +0300

    add sprite and some fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')();
 
+var merge = require('merge-stream');
+
 gulp.task('sass', function() {
     gulp.src('./css/style.scss')
         .pipe($.sourcemaps.init())
@@ -8,13 +10,58 @@ gulp.task('sass', function() {
             errLogToConsole: true
         }))
         .pipe($.sourcemaps.write())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css/'))
+        .pipe($.minifyCss())
+        .pipe($.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('sprite', function() {
+    var spriteData =
+        gulp.src('./img/icons/**/*') // путь, откуда берем картинки для спрайта
+            .pipe($.spritesmith({
+                imgName: '../img/sprite.png',
+                cssName: '_sprite.scss',
+                cssTemplate: './css/_sprite.scss.handlebars',
+                padding: 5,
+                'functions': true,
+                'variableNameTransforms': ['dasherize']
+            }));
+
+    var imgStream = spriteData.img
+        .pipe($.imagemin())
+        .pipe(gulp.dest('./img/'));
+
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./css/'));
+
+    return merge(imgStream, cssStream);
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src(['./img/**/*', '!img/icons/**/*'])
+        .pipe($.imagemin())
+        .pipe(gulp.dest('./img/'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./js/main.js')
+        .pipe(gulp.dest('./js/'))
+        .pipe($.uglifyjs())
+        .pipe($.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./js/'));
 });
 
 // Watch scss folder for changes
 gulp.task('watch', function() {
     gulp.watch('css/**/*.{scss,sass}', ['sass']);
+    gulp.watch('img/icons/**/*', ['sprite']);
+    gulp.watch(['img/**/*', '!img/icons/**/*', '!img/sprite.png'], ['imagemin']);
 });
 
 // Creating a default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['watch', 'sass', 'sprite']);
